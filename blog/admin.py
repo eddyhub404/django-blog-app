192e34d4338937c88b7303d4798795760a1a32b5
eddyhub404 <sharma.adesh019@gmail.com>
2026-06-07 18:33:23 +0530

    Model Register

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'is_published']
+    list_filter = ['category', 'is_published']
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['name', 'email', 'body']
--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,54 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+    
+    def __str__(self):
+        return self.title
+    
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'slug': self.slug})
+    
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f'Comment by {self.name}'
+    
+    
+
+
+
 
