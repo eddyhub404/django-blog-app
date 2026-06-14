c8793ec5646438e56f5228e0f6cbcc8a3cc0d207
eddyhub404 <sharma.adesh019@gmail.com>
2026-06-14 20:05:31 +0530

    Fix welcome email signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,18 +1,3 @@
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
-from django.conf import settings
-
-
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.dispatch import receiver
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
-from django.conf import settings
-
-
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
@@ -24,6 +9,7 @@ from django.conf import settings
 @receiver(post_save, sender=User)
 def send_welcome_mail(sender, instance, created, **kwargs):
     if created and instance.email:
+
         html_content = render_to_string(
             'emails/welcome_email.html',
             {'user': instance}
