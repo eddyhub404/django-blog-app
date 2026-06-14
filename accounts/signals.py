fcdbe679bae1f27d924c1df2ae0b209c5fa661d9
eddyhub404 <sharma.adesh019@gmail.com>
2026-06-14 19:48:04 +0530

    V1

--- accounts/signals.py
+++ accounts/signals.py
@@ -13,11 +13,17 @@ from django.template.loader import render_to_string
 from django.conf import settings
 
 
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
+
+
 @receiver(post_save, sender=User)
 def send_welcome_mail(sender, instance, created, **kwargs):
-
-    if created:
-
+    if created and instance.email:
         html_content = render_to_string(
             'emails/welcome_email.html',
             {'user': instance}
@@ -31,4 +37,4 @@ def send_welcome_mail(sender, instance, created, **kwargs):
         )
 
         email.attach_alternative(html_content, "text/html")
-        email.send()
+        email.send(fail_silently=False)
